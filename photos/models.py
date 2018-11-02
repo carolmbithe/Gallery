75a3ddd6dad79fd3b8ae9ee2b583f3eccfc287c5
carolmbithe <carombithe97@gmail.com>
2018-11-02 11:24:37 +0300

    initial migration

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    # image=models 
+    image_name=models.CharField(max_length =30)
+    image_description=models.CharField(max_length=30)
+    # image location
+    # image category
