4e10beac422a90912526a129878b8842b070b251
carolmbithe <carombithe97@gmail.com>
2018-11-02 09:29:19 +0300

    Added dynamic urls

--- photos/urls.py
+++ photos/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name='index'),
+    url('^today/$',views.today_photos,name='photosToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_photos,name = 'pastPhotos')
 ]
