706c66fd04dc83b1be523aef55a68f73ee8358dc
carolmbithe <carombithe97@gmail.com>
2018-11-02 15:13:17 +0300

    Models

--- photos/models.py
+++ photos/models.py
@@ -1,9 +1,58 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def save_category(self):
+        self.save()
+
+class Location(models.Model):
+    name=models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
 class Image(models.Model):
-    # image=models 
-    image_name=models.CharField(max_length =30)
-    image_description=models.CharField(max_length=30)
-    # image location
-    # image category
+    # image=models
+    name=models.CharField(max_length =30)
+    description=models.TextField()
+    category=models.ForeignKey(Category)
+    location=models.ForeignKey(Location)
+
+    def save_image(self):
+        self.save()
+    # def delete_image(self):
+    #     Image.objects.filter(id=2).delete()
+    #
+    # def update_image:
+    #     Image.objects.filter(id=2).update(description='sth')
+    #
+    # @classmethod
+    # def get_image_by_id(cls,id):
+    #     image=Image.objects.filter(id=)
+    # def search_image():
+    #
+    # @classmethod
+    # def get_image_by_id(cls,id):
+    #
+    # @classmethod
+    # def filter_by_location
+
+    @classmethod
+    def get_photos(cls,id):
+
+        photos=Editor.objects.all()
+        return photos
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from .models import Location,Category,Image
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.design=Category(name='Design')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.design,Category))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.design.save_category()
+        categories=Category.objects.all()
+        self.assertTrue(len(categories)>0)
+
+class LocationTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.nairobi=Location(name='Nairobi')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.nairobi.save_location()
+        locations=Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new category and saving it
+        self.design= Category(name = 'Design')
+        self.james.save_category()
+
+        # Creating a new location and saving it
+        self.nairobi= Location(name = 'Nairobi')
+        self.nairobi.save_location()
+
+        self.new_image= Image(name = 'Test Image',description = 'This is a random test Image description',category = 'design',location='nairobi')
+        self.new_image.save()
+
+        self.new_image.category.add(self.new_category)
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
