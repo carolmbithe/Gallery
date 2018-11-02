895732fcb7d0716528f4c14d28060bb4f934bf1e
carolmbithe <carombithe97@gmail.com>
2018-11-02 08:29:11 +0300

    Template inheritance

--- photos/views.py
+++ photos/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    title="Personal-Gallery"
+    return render(request,'index.html',title=title)
